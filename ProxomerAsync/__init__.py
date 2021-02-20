30b84f9b4f05dc19ea2f344d25da218a7ce81e49
CYPT71 <51490865+CYPT71@users.noreply.github.com>
2021-02-20 20:14:27 +0100

    Add files via upload

---
+++ ProxomerAsync/__init__.py
@@ -0,0 +1,7 @@
+# code modification from https://github.com/baseblack/Proxmoxia/blob/master/
+from proxmox import Connector, Proxmox
+from nodes import Node
+
+__author__ = "Andrew Bunday"
+__copyright__ = "Baseblack Ltd 2012"
+__version__ = "1.0.1"
